3b1886419e5ca3d5d4f444bb700a688c01367ea3
Mara Mulato <maramulato@MacBook-Mara.local>
2022-06-06 21:54:08 -0500

    Add revogrid

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld 
+      :msg="msg"/>
+    <RevoGrid />  
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import RevoGrid from './components/RevoGrid.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    RevoGrid
+  },
+  data () {
+    return {
+    msg: 'Ternure68'
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/RevoGrid.vue
@@ -0,0 +1,57 @@
+<template>
+  <div id="app">
+    <v-grid
+      class="tabla"  
+      theme="compact"
+      :source="rows"
+      :columns="columns"
+    ></v-grid>
+  </div>
+</template>
+ 
+<script>
+import VGrid from "@revolist/vue-datagrid";
+
+const columns = [{
+  name: 'Person name',
+  prop: 'name',
+
+  // use this to return custom html per column
+  columnTemplate: (createElement, column) => {
+    return createElement('span', {
+      style: {
+        color: 'red'
+      },
+    }, column.name);
+  },
+}];
+
+export default {
+  name: "RevoGrid",
+  components: {
+    VGrid,
+  },
+  data() {
+    return {
+      columns: [],
+      rows: [{
+        name: "1",
+        details: "Item 1",
+      }],
+    };
+  },
+  created () {
+      this.assignCol()
+  },
+  methods: {
+      assignCol() {
+          this.columns = columns
+      }
+  }
+};
+</script>
+<style scoped>
+  .tabla {
+      height: 900px;
+  }
+</style>
